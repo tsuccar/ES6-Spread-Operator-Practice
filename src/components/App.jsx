6616303fca1958368bc1632c132fdb7188505283
tsuccar <tsuccar@users.noreply.github.com>
2022-05-17 00:32:45 -0400

    Completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [list, setList] = useState([]);
+  function handleChange(event) {
+    const newItem = event.target.value;
+    console.log(newItem);
+    setItem(newItem);
+  }
+
+  function handleClick() {
+    setList((preValue) => {
+      console.log(preValue);
+      console.log(item);
+      return [...preValue, item];
+    });
+    // event.preventDefault()   just incase needed later with form
+    setItem(""); // just to clear previous text in the imput
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input value={item} onChange={handleChange} type="text" />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {list.map((listItem) => (
+            <li>{listItem}</li>
+          ))}
         </ul>
       </div>
     </div>
---
+++ src/components/ItemList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ItemList(props) {
+  return (
+    <ul>
+      <li>{props}</li>
+    </ul>
+  );
+}
+
+export default ItemList;
